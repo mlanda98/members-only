268c8d0cc94f4241f30b2a9c5004e0f1c545e3af
Mayra Landa <mlandae16@gmail.com>
2025-04-14 10:39:55 -0700

    update for deployments

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.get("/", (req, res) => {
 
 app.set("views", __dirname + "/views");
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
 })
